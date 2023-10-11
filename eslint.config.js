66f7eeff5489d40b391935562ec4719d3daa331b
Jason Garber <jason@sixtwothree.org>
2023-10-10 21:56:24 -0400

    Disable some unicorn rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,12 @@
-module.exports = require('@jgarber/eslint-config');
+const config = require('@jgarber/eslint-config');
+
+module.exports = [
+  ...config,
+  {
+    files: ['**/*.js'],
+    rules: {
+      'unicorn/better-regex': 'off',
+      'unicorn/filename-case': 'off'
+    }
+  }
+];
