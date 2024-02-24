5d265b5be0ab6603f0c7bdeb2f61c0b6a23f3d8a
Jason Garber <jason@sixtwothree.org>
2024-02-24 00:07:38 -0500

    Ignore _site folder

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ module.exports = (async () => {
   const { default: config } = await import("@jgarber/eslint-config");
 
   return [
-    { ignores: ["coverage"] },
+    { ignores: ["_site"] },
     ...config,
     {
       files: ["lib/**/*.js"],
