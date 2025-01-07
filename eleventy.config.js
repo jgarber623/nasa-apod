b575bca825ef1471b9fc3c81c33114121f237e3b
Jason Garber <jason@sixtwothree.org>
2025-01-06 22:36:41 -0500

    Rename webmanifest, use readFileSync instead of import attributes for now

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,5 @@
+import { readFileSync } from "node:fs";
+
 import { EleventyHtmlBasePlugin, EleventyRenderPlugin } from "@11ty/eleventy";
 
 import eleventyPluginLiquid from "@jgarber/eleventy-plugin-liquid";
@@ -5,22 +7,17 @@ import eleventyPluginMarkdown from "@jgarber/eleventy-plugin-markdown";
 
 import youtubeEmbedShortcode from "./lib/shortcodes/youtube_embed.js";
 
-import manifest from "./src/manifest.webmanifest.json" with { type: "json" };
-
 export default function(eleventyConfig) {
   // Front Matter Data
   eleventyConfig.setFrontMatterParsingOptions({ language: "json" });
 
   // Global Data
-  eleventyConfig.addGlobalData("app", manifest);
+  eleventyConfig.addGlobalData("app", JSON.parse(readFileSync("./src/manifest.webmanifest")));
 
   // Passthrough File Copy
   eleventyConfig
     .addPassthroughCopy("./src/assets")
-    .addPassthroughCopy("./src/favicon.ico")
-    .addPassthroughCopy({
-      "./src/manifest.webmanifest.json": "manifest.webmanifest",
-    });
+    .addPassthroughCopy("./src/*.{ico,webmanifest}");
 
   // Shortcodes
   eleventyConfig.addAsyncShortcode("youtube_embed", youtubeEmbedShortcode);
